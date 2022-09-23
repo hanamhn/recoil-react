138d87fe3d989acf0e6aad3350a07d112aec6e80
hanamhn <hanamhndev@gmail.com>
2022-09-23 13:28:59 +0700

    to do list app

--- src/App.js
+++ src/App.js
@@ -49,6 +49,39 @@ const filteredTodoListState = selector({
   },
 });
 
+const todoListStatsState = selector({
+  key: "TodoListStats",
+  get: ({ get }) => {
+    const todoList = get(todoListState);
+    const totalNum = todoList.length;
+    const totalCompletedNum = todoList.filter((item) => item.isComplete).length;
+    const totalUncompletedNum = totalNum - totalCompletedNum;
+    const percentCompleted =
+      totalNum === 0 ? 0 : (totalCompletedNum / totalNum) * 100;
+
+    return {
+      totalNum,
+      totalCompletedNum,
+      totalUncompletedNum,
+      percentCompleted,
+    };
+  },
+});
+
+const TodoListStats = () => {
+  const { totalNum, totalCompletedNum, totalUncompletedNum, percentCompleted } =
+    useRecoilValue(todoListStatsState);
+
+  const formattedPercentCompleted = Math.round(percentCompleted);
+  return (
+    <ul>
+      <li>Total items: {totalNum}</li>
+      <li>Items completed: {totalCompletedNum}</li>
+      <li>Items not completed: {totalUncompletedNum}</li>
+      <li>Percent completed: {formattedPercentCompleted}</li>
+    </ul>
+  );
+};
 const TodoItemCreator = () => {
   const [inputValue, setInputValue] = useState("");
   const setTodoList = useSetRecoilState(todoListState);
@@ -118,15 +151,32 @@ const TodoItem = ({ item }) => {
   );
 };
 
+const TodoListFilters = () => {
+  const [filter, setFilter] = useRecoilState(todoListFilterState);
+
+  const updateFilter = ({ target: { value } }) => {
+    setFilter(value);
+  };
+  return (
+    <>
+      Filter:
+      <select value={filter} onChange={updateFilter}>
+        <option value="Show All">All</option>
+        <option value="Show Completed">Completed</option>
+        <option value="Show Uncompleted">Uncompleted</option>
+      </select>
+    </>
+  );
+};
+
 const TodoList = () => {
   const todoList = useRecoilValue(todoListState);
   console.log(todoList);
   return (
     <>
-      {/* <TodoListStats /> */}
-      {/* <TodoListFilters /> */}
+      <TodoListStats />
+      <TodoListFilters />
       <TodoItemCreator />
-
       {todoList.map((todoItem) => (
         <TodoItem key={todoItem.id} item={todoItem} />
       ))}
